7185524a1c37fa1a30eff7df2b27660747047a08
David <davidmiura@outlook.com.br>
2022-04-26 18:53:01 -0300

    Initial commit

---
+++ curriculo_skills.js
@@ -0,0 +1,25 @@
+let hardskills = ['Java', 'Spring Boot', 'Angular', 'HTML5', 'CSS3', 'JavaScript', 'Python', 'C# Sharp', 'MySQL', 'WordPress', 'SEO', 'Photoshop', 'CorelDraw', 'Blender'];
+
+document.getElementById('listahard').innerHTML = 
+hardskills.map(item => `<li> ${item} </li>`).join('');
+
+
+let softskills = ['', ]
+
+ 
+
+
+
+// Criatividade
+// Empatia
+// Colaboração
+// Adaptação
+// Aprender a aprender
+// Organização
+// Planejamento
+// Desenvolvimento Pessoal
+// Dedicação
+// Flexibilidade
+// Trabalho em Equipe
+// Resiliência
+// Ética
---
+++ menu_script.js
@@ -0,0 +1,20 @@
+const menuIcon = document.getElementById('hamburguer-menu');
+const navbar = document.getElementById('navbar');
+
+menuIcon.addEventListener("click", () => {
+    navbar.classList.toggle('change')
+});
+
+
+
+
+// function clickMenu(){
+//     document.getElementById('hamburguer-menu').classList.toggle("change");
+// }
+
+//Funciona também - Mas tem que colocar a função no Hamburger-menu
+
+    
+    
+    
+
